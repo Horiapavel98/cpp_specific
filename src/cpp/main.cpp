317c9463a5eb78907b70d15f82463849756eef52
Horia <horiapavel@yahoo.com>
2019-04-03 23:31:19 +0300

    Change .hpp to .cpp extensions

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,5 +1,5 @@
 #include "../include/all_custom_headers.hpp"
 
 int main() {
-    MultipleInheritance::output();
+    PersonInheritance::output();
 }
--- src/include/all_custom_headers.hpp
+++ src/include/all_custom_headers.hpp
@@ -1,8 +1,8 @@
 /** Including headers created by author
  * author: horia
  */
-#include "person.hpp"
-#include "access_modifier.hpp"
-#include "override.hpp"
-#include "multi_level_inheritance.hpp"
-#include "multiple_inheritance.hpp"
+#include "person.cpp"
+#include "access_modifier.cpp"
+#include "override.cpp"
+#include "multi_level_inheritance.cpp"
+#include "multiple_inheritance.cpp"
