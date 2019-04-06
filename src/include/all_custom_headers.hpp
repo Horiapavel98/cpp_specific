66d4a1fd34437d067ff0202debc5313c2dffeef2
Horia <horiapavel@yahoo.com>
2019-04-06 14:38:24 +0300

    Delete unnecessary files

--- src/include/all_custom_headers.hpp
+++
@@ -1,8 +0,0 @@
-/** Including headers created by author
- * author: horia
- */
-#include "person.cpp"
-#include "access_modifier.cpp"
-#include "override.cpp"
-#include "multi_level_inheritance.cpp"
-#include "multiple_inheritance.cpp"
--- src/include/default_headers.hpp
+++
@@ -1,6 +0,0 @@
-/* Default include statements */
-#include <iostream>
-#include <string>
-
-/* Namespace std */
-using namespace std;
